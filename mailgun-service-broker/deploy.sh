d7de0ad3fc349eb315762635bd4923e22d60a5f4
Josh Long <josh@joshlong.com>
2017-07-09 18:31:31 -0700

    updated

--- mailgun-service-broker/deploy.sh
+++ mailgun-service-broker/deploy.sh
@@ -7,14 +7,14 @@ service_broker_db=${service_broker}-db
 
 
 cf create-service cleardb spark ${service_broker_db};
+
 cf push -p target/${service_broker}.jar --no-start ${service_broker}
+cf bind-service ${service_broker} ${service_broker_db}
+
 cf set-env ${service_broker} MAILGUN_API_KEY ${MAILGUN_API_KEY}
 cf set-env ${service_broker} MAILGUN_DOMAIN ${MAILGUN_DOMAIN}
-
 cf start ${service_broker}
 
 uri=http://mailgun-service-broker.cfapps.io/
-
 cf delete-service-broker -f ${service_broker}
 cf create-service-broker ${service_broker} admin admin $uri --space-scoped
-
